480b09a7ac20b8f0018e9b662168dd5205645d00
bhanu-0204 <bbhanusree59@gmail.com>
2025-06-12 14:11:42 +0530

    Time: 0 ms (100%), Space: 40.4 MB (64.86%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,9 @@
-public class Solution {
-    public static int fib(int n) {
-        if (n == 0) return 0;   
-        if (n == 1) return 1;  
-        return fib(n - 1) + fib(n - 2); 
-        }
-    public static void main(String[] args) {
-        int n = 7; 
-        System.out.println("Fibonacci of " + n + " is: " + fib(n));
+class Solution {
+    int[] dp = new int[31];
+    public int fib(int n) {
+        if (n <= 1) return n;
+        if (dp[n] != 0) return dp[n];
+        dp[n] = fib(n - 1) + fib(n - 2);
+        return dp[n];
     }
 }
